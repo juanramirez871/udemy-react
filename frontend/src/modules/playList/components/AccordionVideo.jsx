59a3c80d3ca6d803c8d4a3abbc82874d27fbe31d
Juan diego <juandiegoramirez098@gmail.com>
2023-09-23 18:28:27 -0500

    feat: :sparkles: loader ass

--- frontend/src/modules/playList/components/AccordionVideo.jsx
+++ frontend/src/modules/playList/components/AccordionVideo.jsx
@@ -5,7 +5,7 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Chapter } from './Chapter';
-
+import "../../../assets/css/loader.css"
 
 export default function AccordionVideo({ modules, idUser }) {
 
@@ -19,31 +19,36 @@ export default function AccordionVideo({ modules, idUser }) {
       <div style={{ marginBottom: "30px", position: "sticky", top: "20px" }} className='topA'>
         {
           modules.length > 0
-          &&
-          (
-            modules.map((chapter, i) => (
-              <Accordion key={i} expanded={expanded === 'panel' + i} onChange={handleChange('panel' + i)}>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel4bh-content"
-                  id={"panel4bh-header" + i}
-                >
-                  <Typography sx={{ width: '98%', flexShrink: 0 }}>{chapter.title}</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <ul>
-                    {
-                      chapter.videos.map((video, ii) => (
-                        <Chapter key={ii} i={i} video={video} ii={ii} idUser={idUser} />
-                      ))
-                    }
-                  </ul>
-                </AccordionDetails>
-              </Accordion>
-            ))
-          )
+            ?
+            (
+              modules.map((chapter, i) => (
+                <Accordion key={i} expanded={expanded === 'panel' + i} onChange={handleChange('panel' + i)}>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel4bh-content"
+                    id={"panel4bh-header" + i}
+                  >
+                    <Typography sx={{ width: '98%', flexShrink: 0 }}>{chapter.title}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <ul>
+                      {
+                        chapter.videos.map((video, ii) => (
+                          <Chapter key={ii} i={i} video={video} ii={ii} idUser={idUser} />
+                        ))
+                      }
+                    </ul>
+                  </AccordionDetails>
+                </Accordion>
+              ))
+            )
+            :
+            <div style={{ alignItems: 'center', display: "flex", flexDirection: "column" }}>
+              <span className="loader"></span>
+            </div>
         }
       </div>
+
     </div>
   );
 }
